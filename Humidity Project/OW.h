062567dc0da050aaf33c8293e20178e0f6e65079
finch18 <pawel.marek87@gmail.com>
2021-01-07 23:05:56 +0100

    Initial version

---
+++ Humidity Project/OW.h
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+
+#define OW_PIN C, 4
+#define GLUE(a, b) a##b
+#define CLR_(what, p, m) (GLUE(what, p) &= ~(1<<(m)))
+#define CLR(what, OW_PIN) CLR_(what, OW_PIN)
+#define SET_(what, p, m) (GLUE(what, p) |= (1<<(m)))
+#define SET(what, OW_PIN) SET_(what, OW_PIN)
+#define GET(OW_PIN) GET_(OW_PIN)
+#define GET_(/*PIN*/p, m) ((GLUE(PIN,p)) & (1<<(m)))
+
+
+void OW_init();
+void OW_ResetPulse();
+bool OW_WaitForPresencePulse();
+bool OW_ReadBit();
